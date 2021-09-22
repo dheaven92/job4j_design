9d0be61dbce7c23d2f25452bd7080cd39373d5f8
Daria V <voronovadaria@gmail.com>
2021-09-22 21:37:05 +0300

    4.0. File [#252491] fix

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -7,15 +7,21 @@ public class Dir {
 
     public static void main(String[] args) {
         File file = new File("c:\\projects");
+        if (isExistingDirectory(file)) {
+            System.out.printf("%s : %s%n", file.getName(), file.length());
+            for (File subfile : Objects.requireNonNull(file.listFiles())) {
+                System.out.printf("%s : %s%n", subfile.getName(), subfile.length());
+            }
+        }
+    }
+
+    private static boolean isExistingDirectory(File file) {
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.printf("%s : %s%n", file.getName(), file.length());
-        for (File subfile : Objects.requireNonNull(file.listFiles())) {
-            System.out.printf("%s : %s%n", subfile.getName(), subfile.length());
-        }
+        return true;
     }
 }
