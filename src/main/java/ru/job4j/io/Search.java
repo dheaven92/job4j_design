41896ab925ee767e4596942c5e8a177042dac91b
Daria V <voronovadaria@gmail.com>
2021-09-26 11:12:27 +0300

    5. Валидация параметров запуска. [#246865 #207523] fix

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -11,22 +11,26 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Root folder and file extension are required. "
-                    + "Usage java -jar dir.jar ROOT_FOLDER FILE_EXTENSION.");
+        if (validateArgs(args)) {
+            search(Paths.get(args[0]), p -> p.toFile().getName().endsWith(args[1]))
+                    .forEach(System.out::println);
         }
-        File dir = new File(args[0]);
-        if (!dir.isDirectory()) {
-            throw new IllegalArgumentException("First argument must be a directory");
-        }
-        Path start = Paths.get(dir.getAbsolutePath());
-        search(start, p -> p.toFile().getName().endsWith(args[1]))
-                .forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static boolean validateArgs(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder and file extension are required. "
+                    + "Usage java -jar dir.jar ROOT_FOLDER FILE_EXTENSION.");
+        }
+        if (!new File(args[0]).isDirectory()) {
+            throw new IllegalArgumentException("First argument must be a directory");
+        }
+        return true;
     }
 }
