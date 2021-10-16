2e2a22fd2ce1c014711578e1a14bf66b453d4f6d
Daria V <voronovadaria@gmail.com>
2021-10-16 10:40:19 +0300

    2. SQL Select by company [#2760 #216448] fix

--- src/main/java/ru/job4j/sql/company.sql
+++ src/main/java/ru/job4j/sql/company.sql
@@ -9,3 +9,21 @@ on c.id = p.company_id
 group by c.name
 order by people_count desc
 limit 1;
+
+-- to get all possible companies with the same max people count
+with _ranked_groups as (
+    with _groups as (
+        select c.name as company_name, count(*) as people_count
+        from company c
+                 join person p
+                      on c.id = p.company_id
+        group by c.name
+    )
+    select company_name,
+           people_count,
+           rank() over (order by people_count desc) as rank
+    from _groups
+)
+select company_name, people_count
+from _ranked_groups
+where rank = 1;
