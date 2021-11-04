a7330c19dec6005e3604630705fc57b140b15624
Daria V <voronovadaria@gmail.com>
2021-11-04 12:42:22 +0300

    0. Принцип открытости закрытости [#4914] fix

--- src/main/java/ru/job4j/ood/ocp/violations/TheOnlyOneNotifier.java
+++ src/main/java/ru/job4j/ood/ocp/violations/TheOnlyOneNotifier.java
@@ -5,6 +5,6 @@ import ru.job4j.ood.srp.violations.Task;
 public final class TheOnlyOneNotifier {
 
     public void notify(Task task) {
-        // some notification logic goes here
+        System.out.println("notify " + task);
     }
 }
